0f65602bd3e05d7b40db1dbfaac6254f77bb1c26
Siddhu <siddharthaesunuri@gmail.com>
2021-10-05 13:52:19 +0530

    register, login and logout completed

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Authcontroller;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +18,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+//Public routes
+Route::post('/register', [AuthController::class,'register']);
+Route::post('/login', [AuthController::class,'login']);
+
+
+//Protected routes
+Route::group(['middleware'=>['auth:sanctum']], function () {
+    Route::post('/logout', [AuthController::class,'logout']);
 });
